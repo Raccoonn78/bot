ff0db70141f7511cd25cef0544942b95a98e8aa3
Миронов Дмитрий <kirabo007@gmail.com>
2024-04-26 20:19:22 +0300

    feat : new test vesion

--- app/button_bar.py
+++ app/button_bar.py
@@ -1,4 +1,9 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+from aiogram.types.web_app_info import WebAppInfo
+# InlineKeyboardButton(text="Open Webview", web_app=WebAppInfo(url="https://example.com/"))
+
+
 menu = [
     [InlineKeyboardButton(text="📝 Генерировать текст-хуекс", callback_data="generate_text"),
     InlineKeyboardButton(text="🖼 Генерировать изображение", callback_data="generate_image")],
@@ -6,7 +11,8 @@ menu = [
     InlineKeyboardButton(text="💰 Баланс-хуянс", callback_data="balance")],
     [InlineKeyboardButton(text="💎 Партнёрская программа хуй там", callback_data="ref"),
     InlineKeyboardButton(text="🎁 Бесплатные токены-хуекены", callback_data="free_tokens")],
-    [InlineKeyboardButton(text="🔎 Помощь (себе помоги)", callback_data="help")]
+    [InlineKeyboardButton(text="🔎 Помощь (себе помоги)", callback_data="help")],
+    [InlineKeyboardButton(text="🔎 Сайт (тестовая версия)", callback_data="website",  web_app=WebAppInfo(url="https://example.com/"))]
 ]
 menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import F, Router, types
 from aiogram.filters import Command
 from aiogram.types import Message
-
+import logging
 import button_bar
 import text
 
@@ -19,26 +19,44 @@ async def menu(msg: Message):
 
 @router.message()
 async def message_handler(msg: Message):
-    await msg.answer(text.fuck)
+    with open('logg.txt', 'a', encoding='utf-8') as f: f.write(f'ПИШЕТ Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}\n')
+    logging.info( f'ПИШЕТ Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}'  )
+    await msg.answer(text.fuck)   
 
 @router.callback_query(F.data == "generate_image")
 async def start_handler_generate_image(msg: Message):
+    with open('logg.txt', 'a', encoding='utf-8') as f: f.write(f'НАЖАЛ кнопку generate_image Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}\n')
+    logging.info( f'НАЖАЛ кнопку generate_image Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}'  )
     await msg.answer(text.fuck, reply_markup=button_bar.menu)
 
 @router.callback_query(F.data == "help")
 async def start_handler_help(msg: Message):
+    with open('logg.txt', 'a', encoding='utf-8') as f: f.write(f'НАЖАЛ кнопку help Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}\n')
+    logging.info( f'НАЖАЛ кнопку help Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}'  )
     await msg.answer(text.fuck, reply_markup=button_bar.menu)
 
 
 @router.callback_query(F.data == "generate_text")
 async def start_handler_generate_text(msg: Message):
+    with open('logg.txt', 'a', encoding='utf-8') as f: f.write(f'НАЖАЛ кнопку generate_text Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}\n')
+    logging.info( f'НАЖАЛ кнопку generate_text Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}'  )
     await msg.answer(text.fuck, reply_markup=button_bar.menu)
 
 @router.callback_query(F.data == "free_tokens")
 async def start_handler_free_tokens(msg: Message):
+    with open('logg.txt', 'a', encoding='utf-8') as f: f.write(f'НАЖАЛ кнопку free_tokens Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}\n')
+    logging.info( f'НАЖАЛ кнопку free_tokens Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}'  )
     await msg.answer(text.fuck, reply_markup=button_bar.menu)
 
 
 @router.callback_query(F.data == "balance")
 async def start_handler_free_tokens(msg: Message):
+    with open('logg.txt', 'a', encoding='utf-8') as f: f.write(f'НАЖАЛ кнопку balance Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}\n')
+    logging.info( f'НАЖАЛ кнопку balance Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}'  )
+    await msg.answer('нет у тебя ничего', reply_markup=button_bar.menu)
+
+@router.callback_query(F.data == "website")
+async def start_handler_free_tokens(msg: Message):
+    with open('logg.txt', 'a', encoding='utf-8') as f: f.write(f'НАЖАЛ кнопку website Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}\n')
+    logging.info( f'НАЖАЛ кнопку website Пользователь: {msg.from_user.full_name} с id {msg.from_user.id}'  )
     await msg.answer('нет у тебя ничего', reply_markup=button_bar.menu)
